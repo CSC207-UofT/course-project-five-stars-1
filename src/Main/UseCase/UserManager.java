071ea078ce8336437cfbea8d9d4a01a7a665090d
Richard202019Wang <82163830+Richard202019Wang@users.noreply.github.com>
2021-10-11 18:26:16 -0400

    refectory the UseManger file

--- src/Main/UseCase/UserManager.java
+++ src/Main/UseCase/UserManager.java
@@ -2,29 +2,30 @@ package UseCase;
 
 import Entity.*;
 import java.util.ArrayList;
+import java.util.Objects;
 
 public class UserManager {
     public static ArrayList<User> userlist;
 
-    public UserManager(ArrayList<IUser> userList){
+    public UserManager(ArrayList<User> userList){
         userlist = userList;
     }
-    public String getUser(String username){
-        for (IUser u : userList){
+    public String getUser(String username, Iterable<? extends User> userList){
+        for (User u : userList){
             if(u.getUsername().equals(username))
                 return u.toString();
         }
-        return "no such user";
+        return "The user doesn't exist.";
     }
     public boolean verifyUser(String id, String password){
-        for (IUser u : userlist){
-            if (u.getUsername(). equal(id) && u.getPassword().equals(password))
+        for (User u : userlist){
+            if (Objects.equals(u.getUsername(), id) && Objects.equals(u.getPassword(id), password))
                 return true;
         }
-        return false
+        return false;
     }
-    public void createRegularUser(String username, String pw){
-        IUSer user = new regularUser(username, pw);
+    public void createEmployee(String username, String password, int salary, boolean attendance, String id){
+        User user = new Employee(username, password, salary, attendance, id);
         userlist.add(user);
     }
 }
