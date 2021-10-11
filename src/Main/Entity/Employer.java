17ccc3632da4ff0143478339f113a894e6375931
jasonli8408 <jasonli8408@gmail.com>
2021-10-11 17:39:57 -0400

    no priviledge

--- src/Main/Entity/Employer.java
+++ src/Main/Entity/Employer.java
@@ -1,7 +1,6 @@
 package Entity;
 
 public class Employer extends User{
-    String privilige;
 
     public Employer(String username, String password, int salary,  boolean Attendance, String ID){
         super(username, password, salary, Attendance, ID );
