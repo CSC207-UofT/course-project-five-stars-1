93d967ebe81b6af6947176556d06dbc1f38072cc
Haarowww <75808020+Haarowww@users.noreply.github.com>
2021-10-14 15:26:47 -0400

    Add files via upload
    gkd

--- ui.java
+++ ui.java
@@ -2,7 +2,7 @@ package UI;
 
 import Controller.UserSystemController;
 import Controller.SalarySystemController;
-import UseCase.SalaryManagement;
+
 
 import java.util.Scanner;
 
@@ -10,20 +10,54 @@ public class ui {
 
     public static void main(String[] args){
         SalarySystemController ssc = new SalarySystemController();
+        UserSystemController usc = new UserSystemController();
 
         Scanner sc = new Scanner(System.in);
 
         while (true){
-            System.out.println("Choose your option");
+            System.out.println("Choose your option. 1 FOR SALARYSYSTEM, 2 FOR USERSYSTEM, 0 FOR EXIT");
             int option = sc.nextInt();
             if (option == 1){
-                System.out.println("ABCDEFG");
+                System.out.println("WELCOME, enter your id");
                 sc.nextLine();
                 String id = sc.nextLine();
                 System.out.println("This is your salary");
                 ssc.showSalary(id);
-            } else {
+            } else if (option == 2){
+                System.out.println("Welcome, enter your option");
+                int pp = sc.nextInt();
+                if (pp == 1){
+                    System.out.println("Enter your username");
+                    sc.nextLine();
+                    String username = sc.nextLine();
+                    System.out.println("Enter your password");
+                    sc.nextLine();
+                    String password = sc.nextLine();
+                    System.out.println("Enter your salary");
+                    int salary = sc.nextInt();
+                    System.out.println("Enter your attandance");
+                    boolean attandance = sc.nextBoolean();
+                    System.out.println("Enter your id");
+                    sc.nextLine();
+                    String id = sc.nextLine();
+                    usc.create(username, password, salary, attandance, id);
+                } else if (pp == 2){
+                    System.out.println("Enter your id");
+                    sc.nextLine();
+                    String id = sc.nextLine();
+                    System.out.println("This is the result");
+                    usc.getUser(id);
+                } else if (pp == 3){
+                    System.out.println("Enter your username");
+                    sc.nextLine();
+                    String username = sc.nextLine();
+                    System.out.println("This is your result");
+                    usc.verify(username);
+                }
+
+            } else if (option == 0){
                 System.out.println("closed");
+                break;
             }
         }
     }
