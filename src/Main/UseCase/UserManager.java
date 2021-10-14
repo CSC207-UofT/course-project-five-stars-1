9ca78e6f6f094dcc2919f9f14af4f82ba6ffdc3b
Skyee9 <shskye.li@mail.utoronto.ca>
2021-10-14 13:56:02 -0400

    Comments for usecase-userManager

--- src/Main/UseCase/UserManager.java
+++ src/Main/UseCase/UserManager.java
@@ -4,12 +4,26 @@ import Entity.*;
 import java.util.ArrayList;
 import java.util.Objects;
 
+/**
+ * Represents the system of Users
+ */
 public class UserManager {
     public static ArrayList<User> userlist;
 
+    /**
+     * Creates a UserManager with an empty list of Users
+     */
     public UserManager(ArrayList<User> userList){
         userlist = userList;
     }
+
+    /**
+     * Getter for the username as a String
+     * @param username User's username.
+     * @param userList A list with Users.
+     * @return a string of username if the username is the same as the username of User in userlist,
+     * else return 'The user doesn't exist.'
+     */
     public String getUser(String username, Iterable<? extends User> userList){
         for (User u : userList){
             if (u.getUsername().equals(username))
@@ -18,6 +32,13 @@ public class UserManager {
         return "The user doesn't exist.";
     }
 
+    /**
+     * Getter for the id as a String.
+     * @param id A unique String for each User.
+     * @param userList A list with Users.
+     * @return a string of id if id is the same as the id of User in userlist,
+     * else return 'The user id doesn't exist.'
+     */
     public String getUserFromId(String id, Iterable<? extends User> userList){
         for (User u : userList){
             if (u.getID().equals(id))
@@ -26,7 +47,12 @@ public class UserManager {
         return "The user id doesn't exist.";
     }
 
-
+    /**
+     * Verify the id is valid.
+     * @param id A unique String for each User.
+     * @return ture if the id is the same as the id of User in userlist,
+     * else return false
+     */
     public boolean verifyUser(String id){
         for (User u : userlist){
             if (Objects.equals(u.getID(), id))
@@ -34,10 +60,28 @@ public class UserManager {
         }
         return false;
     }
+
+    /**
+     * Adds an employee with username, password, salary, attendance, and id into userlist.
+     * @param username User's username.
+     * @param password User's password.
+     * @param salary User's salary.
+     * @param attendance User's attendance.
+     * @param id A unique String for each User.
+     */
     public void createEmployee(String username, String password, int salary, boolean attendance, String id){
         User user = new Employee(username, password, salary, attendance, id);
         userlist.add(user);
     }
+
+    /**
+     * Adds an employer with username, password, salary, attendance, and id into userlist.
+     * @param username User's username.
+     * @param password User's password.
+     * @param salary User's salary.
+     * @param attendance User's attendance.
+     * @param id A unique String for each User.
+     */
     public void createEmployer(String username, String password, int salary, boolean attendance, String id){
         User user = new Employer(username, password, salary, attendance, id);
         userlist.add(user);
