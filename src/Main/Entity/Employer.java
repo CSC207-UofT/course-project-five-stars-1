7cfe319b9209e72ed31a61d13151731d50bd805a
suprrray <Jiangruijian123>
2021-10-14 13:28:13 -0400

    comment for entity

--- src/Main/Entity/Employer.java
+++ src/Main/Entity/Employer.java
@@ -1,11 +1,18 @@
 package Entity;
 
+/**
+ * Represents the entire system of Employer(one of the User)
+ */
 public class Employer extends User{
 
     public Employer(String username, String password, int salary,  boolean attendance, String id){
         super(username, password, salary, attendance, id );
     }
 
+    /**
+     * Represents the current Employer by their Username, Salary, Attendance and ID
+     * @return the username, salary, attendance and id of current User
+     */
     @Override
     public String toString(){
         return "Employer {" + "Username=" + getUsername() + ", Salary = " + getSalary() + ", Attendance = "
