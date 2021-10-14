56ccc18c5feaff88bc5caa2e966bd02ac9a1b1eb
Skyee9 <shskye.li@mail.utoronto.ca>
2021-10-14 16:36:45 -0400

    Comments for controller

--- src/Main/Controller/SalarySystemController.java
+++ src/Main/Controller/SalarySystemController.java
@@ -4,11 +4,22 @@ import UseCase.SalaryManagement;
 
 import java.util.HashMap;
 
+/**
+ * Controls the SalaryManagement system.
+ */
 public class SalarySystemController {
     private SalaryManagement salaryManagement;
 
-    public SalarySystemController(){this.salaryManagement = new SalaryManagement();}
+    public SalarySystemController(){
+        this.salaryManagement = new SalaryManagement();
+    }
 
+
+    /**
+     * Print out the salary based on the provided id.
+     * Otherwise, print "Sorry, there does not exist your information"
+     * @param id A unique String for each User.
+     */
     public void showSalary(String id){
         if (salaryManagement.getSm().containsKey(id)){
             System.out.println("This is your salary for this month:");
