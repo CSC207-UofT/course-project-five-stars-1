0e85cd065633cc8a9cbaec9b7bf596e64f99d9f7
Richard202019Wang <82163830+Richard202019Wang@users.noreply.github.com>
2021-11-11 18:39:17 -0500

    did a lot of changes

---
+++ src/Main/Controller/JobpSystemController.java
@@ -0,0 +1,21 @@
+package Controller;
+import UseCase.Jobposition;
+import java.util.Objects;
+
+public class JobpSystemController {
+    private Jobposition jobposition;
+    public JobpSystemController(){this.jobposition = new Jobposition();}
+    public Jobposition getJobposition(){
+        return jobposition;
+    }
+    public boolean addMajor(String id, String major){
+        return jobposition.addMajor(id, major);
+    }
+    public String getMajor(String id){
+        return jobposition.getMajor(id);
+    }
+    public String getReference(String major){
+        System.out.println("Here is the job position we recommend to you: ");
+        return jobposition.getReference(major);
+    }
+}
