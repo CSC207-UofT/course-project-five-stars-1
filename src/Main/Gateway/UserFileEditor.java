531de47b0bfa9fbb2baaf5bd1160b3e1833a13e6
jasonli8408 <jasonli8408@gmail.com>
2021-11-11 22:01:38 -0500

    serializable class and wildcard

--- src/Main/Gateway/UserFileEditor.java
+++ src/Main/Gateway/UserFileEditor.java
@@ -5,7 +5,7 @@ import java.util.ArrayList;
 
 public class UserFileEditor {
 
-    public void saveUser(ArrayList userList){
+    public void saveUser(ArrayList<?> userList){
         try{
             FileOutputStream fos = new FileOutputStream("UserData");
             ObjectOutputStream oos = new ObjectOutputStream(fos);
@@ -18,16 +18,17 @@ public class UserFileEditor {
         }
     }
 
-    public ArrayList readUser(){
+    public ArrayList<?> readUser(){
 
-        ArrayList userList = new ArrayList<>();
+        ArrayList<?> userList = new ArrayList<>();
 
         try{
             File file = new File("Userdata");
             file.createNewFile(); // create the file if the file path exists no file
             FileInputStream fis = new FileInputStream(file);
             ObjectInputStream ois = new ObjectInputStream(fis);
-            userList = (ArrayList) ois.readObject();
+            userList = (ArrayList<?>) ois.readObject();
+            System.out.println(userList); //test if it worked
             ois.close();
             fis.close();
 
