c70ba44c6f8d052b6decfc10c0865725b991e511
Michelin <jiangyuelin1124@gmail.com>
2021-11-14 02:19:39 -0500

    sth

--- src/Main/Controller/InternSystemController.java
+++ src/Main/Controller/InternSystemController.java
@@ -7,7 +7,9 @@ import UseCase.InternManagement;
 import java.io.IOException;
 import java.util.HashMap;
 import java.util.Objects;
-
+/**
+ * Controls the Intern system.
+ */
 public class InternSystemController {
     private final InternManagement internManagement;
 
@@ -16,11 +18,26 @@ public class InternSystemController {
     public InternSystemController(HashMap<Intern, Integer> intern){
         this.internManagement = new InternManagement(intern);
     }
-
+    /**
+     * Getter internManagement
+     * @return internManagement.
+     */
     public HashMap<Intern, Integer> getOurMap() {
         return internManagement.getIm();
     }
 
+
+    /**
+     * Print out you are registered.
+     * Otherwise, print "You do not have the authority to edit this file"
+     * @param id1 A unique String for each User.
+     * @param id2 A unique String for each User.
+     * @param username A unique String for each User.
+     * @param password A unique String for each User.
+     * @param email A unique String for each User.
+     * @param major A unique String for each User.
+     * @param grade A unique integer for each User.
+     */
     public String registerInformation(String id1, String id2, String username, String password,
                                     String email, String major, int grade){
         if (Objects.equals(id1, "0")){
@@ -30,7 +47,12 @@ public class InternSystemController {
             return "You do not have the authority to edit this file";
         }
     }
-
+    /**
+     * Print out the file is edited successfully.
+     * Otherwise, print "Sorry, you do not have the authority to edit this file"
+     * @param id A unique String for each User.
+     * @param standard A unique Integer for each User.
+     */
     public String updateInformation(String id, int standard){
         if (Objects.equals(id, "0")){
             this.internManagement.updateInformation(standard);
@@ -39,7 +61,10 @@ public class InternSystemController {
             return "Sorry, you do not have the authority to edit this file";
         }
     }
-
+    /**
+     * Getter internManagement
+     * @return internManagement.
+     */
     public boolean checkInformation(String id){
         return this.internManagement.checkInformation(id);
     }
--- src/Main/Controller/JobpSystemController.java
+++ src/Main/Controller/JobpSystemController.java
@@ -6,18 +6,34 @@ import java.util.HashMap;
 
 public class JobpSystemController {
     private final Jobposition jobposition;
+    /**
+     * Getter jobposition
+     * create new jobposition.
+     */
     public JobpSystemController(HashMap<String, String> jobs){
         this.jobposition = new Jobposition(jobs);
     }
+    /**
+     * Getter jobposition
+     * @return jobposition.
+     */
     public Jobposition getJobposition(){
         return jobposition;
     }
+    /**
+     * Getter jobposition
+     * @return jobposition.addMajor.
+     */
     public String addMajor(String id, String major){
         return jobposition.addMajor(id, major);
     }
     public String getMajor(String id){
         return jobposition.getMajor(id);
     }
+    /**
+     * Getter jobposition
+     * @return str
+     */
     public String getReference(String major){
         System.out.println("Here is the job position we recommend to you: ");
         return jobposition.getReference(major);
