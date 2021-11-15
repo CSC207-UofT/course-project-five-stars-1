c9c2ac5a80c092b75d10e73ea4b9f2ebcdae3e07
Skyee9 <shskye.li@mail.utoronto.ca>
2021-11-15 07:37:15 -0500

    Fixed docstring in Jobpositioncontroller and ScheduleSystemController

--- src/Main/Controller/ScheduleSystemController.java
+++ src/Main/Controller/ScheduleSystemController.java
@@ -1,42 +1,51 @@
 package Controller;
-
 import UseCase.WeeklySchedule;
-
 import java.util.HashMap;
 
+/**
+ * Controls the WeeklySchedule system.
+ */
 public class ScheduleSystemController {
     private final WeeklySchedule weeklySchedule;
-    /**
-     * Getter weeklySchedule
-     * create new weeklySchedule.
-     */
+
     public ScheduleSystemController(HashMap<String, Integer> schedules){
         this.weeklySchedule = new WeeklySchedule(schedules);
     }
+
     /**
-     * Getter weeklySchedule
-     * @return weeklySchedule.
+     * Getter for WeeklySchedule
+     * @return Weekly Schedule
      */
     public WeeklySchedule getWeeklySchedule() {
         return this.weeklySchedule;
     }
+
     /**
-     * Getter weeklySchedule
-     * @return weeklySchedule.addSchedule.
+     * Add schedule into system based on the provided ID
+     * @param id user's ID
+     * @return "Cannot add your schedule since your information exists" if ID exists in system.
+     * Add user into system with 40 hours schedule and
+     * return "Add your schedule successfully" if ID does not exist in system
      */
     public String addSchedule(String id){
         return this.weeklySchedule.addSchedule(id);
     }
+
     /**
-     * Getter weeklySchedule
-     * @return weeklySchedule.getHour.
+     * Get the hours based on the provided ID
+     * @param id user's ID
+     * @return the hours as an integer based on the ID
      */
     public int checkSchedule(String id){
         return this.weeklySchedule.getHour(id);
     }
+
     /**
-     * Getter weeklySchedule
-     * @return weeklySchedule.changeSchedule.
+     * Change the schedule
+     * @param id user's ID
+     * @param changeHour changing hour
+     * @return "Failed to update your schedule " if ID does not exist in the system.
+     * modify the schedule through provided ID and return "Update your schedule successfully" if ID exists.
      */
     public String changeSchedule(String id, int changeHour){
         return this.weeklySchedule.changeSchedule(id, changeHour);
